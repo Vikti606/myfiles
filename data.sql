b50674e47a182a221462232ec0a9008581f8d569
Виктория <94623750+Vikti606@users.noreply.github.com>
2022-01-25 20:25:06 +0300

    Add files via upload

---
+++ data.sql
@@ -0,0 +1,11 @@
+insert into usr (first_name, middle_name, last_name, email) values
+('Иван', 'Иванович', 'Иванов', 'ivanov@my.example.com'),
+('Петр', 'Петрович', 'Петров', 'petrov@my.example.com'),
+('Иван', 'Петрович', 'Сидоров', 'sidorov@my.example.com'),
+('Виктор', 'Иванович', 'Потёмкин', 'potemkin@my.example.com'),
+('Петр', 'Васильевич', 'Ковалёв', 'kovalev@my.example.com'),
+('Илья', 'Петрович', 'Соболев', 'sobolev@my.example.com'),
+('Николай', 'Артёмович', 'Никитин', 'nikitin@my.example.com'),
+('Максим', 'Андреевич', 'Солдатов', 'soldatov@my.example.com'),
+('Иван', 'Петрович', 'Кремлев', 'kremlev@my.example.com'),
+('Иван', 'Петрович', 'Сорокин', 'sorokin@my.example.com');
